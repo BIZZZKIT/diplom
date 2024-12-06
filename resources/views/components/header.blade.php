7d3fafba9e1058dd58e6d783855feae9f3b53557
Rozen Maxim <max.r2005@gmail.com>
2024-12-06 22:08:22 +0500

    До блока фильтрации главной страницы

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,103 @@
+<style>
+
+    body {
+        margin: 0;
+        padding: 0;
+        background-color: #000;
+        color: white;
+    }
+
+    a, li {
+        font-family: 'Montserrat', sans-serif;
+        color: white !important;
+        font-size: 20px;
+        font-weight: inherit;
+    }
+
+    .dropdown-menu {
+        background-color: black;
+        border: none;
+    }
+
+    .dropdown-item {
+        font-size: 16px;
+        font-weight: normal;
+        color: white !important;
+    }
+
+    .dropdown-item:hover {
+        background-color: #5B5400;
+    }
+
+    .navbar-brand img {
+        width: 150px;
+    }
+
+    .navbar {
+        height: 80px;
+    }
+
+    .container-fluid {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .navbar-nav {
+        flex-grow: 1;
+        justify-content: center;
+        padding-right: 80px;
+    }
+
+    .account {
+        display: flex;
+        align-items: center;
+    }
+</style>
+
+<div class="container">
+    <nav class="navbar navbar-expand-lg" style="background: none; padding-top: 18px;">
+        <div class="container-fluid">
+
+            <!-- Логотип слева -->
+            <a class="navbar-brand" href="#">
+                <img src="{{asset('assets/images/logo.png')}}" alt="Logo">
+            </a>
+
+            <!-- Кнопка-тогглер -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Центральные вкладки -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Каталог</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">О компании</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Контакты</a>
+                    </li>
+                </ul>
+
+                <!-- Аккаунт справа -->
+                <div class="account">
+                    <li class="nav-item dropdown" style="list-style: none;">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img width="50px" src="{{asset('assets/images/user.png')}}" alt="User">
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,112 @@
+<style>
+    .formSearch {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        background-image: url('{{asset('assets/images/forformmain.jpg')}}');
+        background-size: cover;
+        background-position: center;
+        height: 704px;
+        color: white;
+    }
+    .formSearch .container form {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 16px;
+    }
+
+    .formSearch .container form .btn {
+        color: black;
+        border: none;
+        background-color: #FFC300;
+        grid-column: 2 / span 2;
+    }
+
+
+    .formSearch::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.73);
+        z-index: 1;
+    }
+
+    .formSearch .container {
+        position: relative;
+        z-index: 2;
+    }
+
+</style>
+<div class="container">
+    <div id="carouselExample" class="carousel slide" style="border-radius: 20px; overflow: hidden; padding-top: 57px; padding-bottom: 58px;">
+        <div class="carousel-inner" style="border-radius: 20px;">
+            <div class="carousel-item active">
+                <img src="{{asset('assets/images/carousel/2f6865a8366bde9d6401bf8877276e7e2e6feb8c.png')}}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{asset('assets/images/carousel/chelyabinsk_manhatten.png')}}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{asset('assets/images/carousel/ярославский.png')}}" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+<div class="formSearch">
+    <div class="container">
+        <h1 style="font-family: 'Montserrat'; font-weight: bold; padding-bottom: 86px;">Найди свою мечту</h1>
+        <form style="font-family: 'Roboto'; font-weight: normal;">
+            <div class="mb-3">
+                <select class="form form-select">
+                    <option>Продажа</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <select class="form form-select">
+                    <option>Регион</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <select class="form form-select">
+                    <option>Квартира</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <select class="form form-select">
+                    <option>Кол-во комнат</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <select class="form form-select">
+                    <option>Город</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <select class="form form-select">
+                    <option>Район</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <input class="form-control" placeholder="От">
+            </div>
+            <div class="mb-3">
+                <input class="form-control" placeholder="До">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+
+
